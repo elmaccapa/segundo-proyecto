59c2607669fe0ba87360f62ed04bb8dc0b1208ab
juan manuel <elmaccapabayona@gmail.com>
2016-03-06 01:05:55 -0500

    segundo proyecto

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title> proyecto elma</title>
+		<link rel="stylesheet" href="public/css/bootstrap.css">
+
+	<link rel="stylesheet" href="estilos.css">
+	<link rel="stylesheet" href="fonts.css">
+</head>
+<body>
+<header>
+	<nav>
+		<ul>
+
+		<li><a href="#"><span class="primero"><i class="icon icon-heart"></i></span>Ininio</a></li>
+		<li><a href="#"><span class="segundo"><i class="icon icon-like"></i></span>Citas Biblicas</a></li><ul>
+</ul>
+</li>
+<li><a href="#"><span class="tercero"><i class="icon icon-star"></i></span>La Fe de Jesus</a></li>
+<li><a href="#"><span class="cuarto"><i class="icon icon-photo"></i></span>Alabanzas</a></li>
+ <li><a href="#"><span class="quinto"><i class="icon icon-settings"></i></span>Oraciones</a></li>
+
+		</ul>
+	</nav>
+	<div id="carousel-id" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+			<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+			<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="item">
+				<img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="imagenes/elma2.jpg">
+				<div class="container">
+					<div class="carousel-caption">
+						
+						<p><b>Acuérdate del señor tu Dios en todo momento, no solo cuando las dificultades lleguen a tu vida y verás como tendrás su Espíritu contigo todo el tiempo y te protegerá a ti y a los tuyos de toda maldad. </b></p>
+						<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="imagenes/elma3.jpg">
+				<div class="container">
+					<div class="carousel-caption">
+						
+						<p>Si alguna vez te llegas a sentir solo debes saber que Dios nunca te desampara, siempre está a tu lado sin importar lo que pase y te conoce mejor que nadie, Él sabe lo que necesitas pero espera que tú se lo pidas en oración con un corazón sincero.</p>
+						<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="item active">
+				<img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" alt="Third slide" src="imagenes/elma 1.jpg">
+				<div class="container">
+					<div class="carousel-caption">
+					<p>Aunque la vida te de mil motivos para renunciar... Dios te da mil y un motivos más para seguir adelante.</p>
+						<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+		<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+	</div>
+	<script src="public/js/jquery-1.12.1.js"></script>
+<script  src="public/js/bootstrap.js"></script>
+<style >
+h1{ 
+  margin: 20px; 
+  border: red 2px solid; 
+}
+
+}
+	
+</style>
+
+<video src="musica/PRECIOSO_JESUS_con_letra.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+
+<video src="musica/FORGIVEN - TIEMPO.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+<video src="musica/Lisbeth Melgar  En las aguas de la muerte.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+
+
+</header>
+
+</body>
+</html>
+
+
